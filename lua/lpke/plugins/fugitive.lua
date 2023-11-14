6396ba9cb8ceeb7b3f7054641167a5bd82124340
Luke <luke@lpdev.io>
2023-11-15 09:32:36 +1100

    installed fugitive

---
+++ lua/lpke/plugins/fugitive.lua
@@ -0,0 +1,10 @@
+local function config()
+  local helpers = require('lpke.core.helpers')
+  local tc = Lpke_theme_colors
+end
+
+return {
+  'tpope/vim-fugitive',
+  event = 'VeryLazy',
+  config = config,
+}
