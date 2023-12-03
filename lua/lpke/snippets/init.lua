87aacd68a438fa71c6d6ea838768c87c9fe432d4
Luke <luke@lpdev.io>
2023-12-04 04:17:56 +1100

    Cleaned up jsx/tsx filenames

--- lua/lpke/snippets/init.lua
+++ lua/lpke/snippets/init.lua
@@ -2,14 +2,14 @@ local ct = require('lpke.core.helpers').concat_tables
 
 local js = require('lpke.snippets.js')
 local ts = require('lpke.snippets.ts')
-local jsreact = require('lpke.snippets.jsreact')
-local tsreact = require('lpke.snippets.tsreact')
+local jsx = require('lpke.snippets.jsx')
+local tsx = require('lpke.snippets.tsx')
 
 return {
   all = require('lpke.snippets.all'),
   lua = require('lpke.snippets.lua'),
   javascript = js,
   typescript = ct(js, ts),
-  javascriptreact = ct(js, jsreact),
-  typescriptreact = ct(js, ts, jsreact, tsreact),
+  javascriptreact = ct(js, jsx),
+  typescriptreact = ct(js, ts, jsx, tsx),
 }
