81fe53c8027754f624b7f188103add201ab96a2c
Luke <luke@lpdev.io>
2023-12-04 05:15:57 +1100

    JSX snippets

--- lua/lpke/snippets/jsx.lua
+++ lua/lpke/snippets/jsx.lua
@@ -6,4 +6,75 @@ local ls, s, _s, sn, t, t_, i, f, d, rep, fmtc, fmta, fmt, sel, sel_q, sel_b, ex
 -- stylua: ignore end
 
 return { -- jsreact
+  _s({
+    trig = 'ii',
+    name = 'Import',
+  }, fmt("import <> from '<>';", { i(1, '{  }'), i(2) })),
+  _s(
+    {
+      trig = 'ue',
+      name = 'useEffect',
+    },
+    fmt(
+      [[
+        useEffect(() =>> {
+          <>
+        }, [<>]);
+      ]],
+      { i(1), i(2) }
+    )
+  ),
+  _s(
+    {
+      trig = 'ued',
+      name = 'useEffect debug',
+    },
+    fmt(
+      [[
+        useEffect(() =>> {
+          console.log({ <> });
+        }, [<>]);
+      ]],
+      { i(1), rep(1) }
+    )
+  ),
+  _s(
+    {
+      trig = 'us',
+      name = 'useState',
+    },
+    fmt(
+      [[
+        const [<>, set<>] = useState(<>);
+      ]],
+      {
+        i(1),
+        f(function(args)
+          return (args[1][1]:gsub('^%l', string.upper))
+        end, { 1 }),
+        i(2),
+      }
+    )
+  ),
+  s({
+    trig = 'cn',
+    name = 'JSX className',
+  }, fmt('className=')),
+  s({
+    trig = '}}',
+    name = 'Template literal expression',
+    dscr = 'Convert quoted text to a curly-brace wrapped template literal',
+  }, fmt('{`<>`}', { d(1, sel_q) })),
+  s({
+    trig = '}}$',
+    name = 'Temperate string interpolation',
+  }, fmt('{`<> ${<>}`}', { d(2, sel_q), i(1) })),
+  s({
+    trig = '}&',
+    name = 'Conditional expression',
+  }, fmt('{<> && (<>)}', { i(1), i(2, '<div></div>') })),
+  s({
+    trig = '}?',
+    name = 'Ternary expression',
+  }, fmt('{<> ? <> : <>}', { i(1), i(2, '()'), i(3, '()') })),
 }
