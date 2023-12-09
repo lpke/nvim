bf9ead251b15385e9b773d37919f2d247a69f16b
Luke <luke@lpdev.io>
2023-12-09 18:19:28 +1100

    Renamed snippets/helpers.lua to snippets/ls_helpers.lua for less ambigutity when searching files

--- lua/lpke/snippets/js.lua
+++ lua/lpke/snippets/js.lua
@@ -1,5 +1,5 @@
 -- stylua: ignore start
-local h = require('lpke.snippets.helpers')
+local h = require('lpke.snippets.ls_helpers')
 ---@diagnostic disable-next-line: unused-local
 local ls, s, _s, sn, t, t_, i, f, d, rep, fmtc, fmta, fmt, sel, sel_q, sel_b, exp_conds =
   h.ls, h.s, h._s, h.sn, h.t, h.t_, h.i, h.f, h.d, h.rep, h.fmtc, h.fmta, h.fmt, h.sel, h.sel_q, h.sel_b, h.exp_conds
