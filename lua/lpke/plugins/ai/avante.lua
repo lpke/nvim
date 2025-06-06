9b7f2f94b5de92d024e32efe95dfd77f8d47fdbe
Luke Perich <luke.p@montu.com.au>
2025-06-06 16:12:05 +1000

    added other model configs

--- lua/lpke/plugins/ai/avante.lua
+++ lua/lpke/plugins/ai/avante.lua
@@ -6,21 +6,9 @@ local function config()
   avante.setup({
     provider = 'claude',
     providers = {
-      openai = {
-        endpoint = 'https://api.openai.com/v1',
-        model = 'gpt-4o', -- your desired model (or use gpt-4o, etc.)
-        extra_request_body = {
-          timeout = 30000, -- ms
-          temperature = 0.75,
-          max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-          --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
-        },
-      },
       claude = {
-        -- modal name | cost input | cost output (/ MTok)
-        -- claude-opus-4-20250514   | $15   | $75
-        -- claude-sonnet-4-20250514 | $3    | $15
-        -- claude-3-5-haiku-latest  | $0.80 | $4
+        -- model names: https://docs.anthropic.com/en/docs/about-claude/models/overview#model-names
+        -- model costs: https://docs.anthropic.com/en/docs/about-claude/models/overview#model-pricing
         endpoint = 'https://api.anthropic.com',
         model = 'claude-sonnet-4-20250514',
         timeout = 45000, -- ms (increase for reasoning models)
@@ -28,6 +16,89 @@ local function config()
           temperature = 0.75,
           max_tokens = 20480,
         },
+      },
+      ['claude-haiku'] = {
+        __inherited_from = 'claude',
+        model = 'claude-3-5-haiku-latest',
+        timeout = 30000,
+        extra_request_body = {
+          temperature = 0.75,
+          max_tokens = 8192,
+        },
+      },
+      ['claude-opus'] = {
+        __inherited_from = 'claude',
+        model = 'claude-opus-4-20250514	',
+        timeout = 60000,
+        extra_request_body = {
+          temperature = 0.75,
+          max_tokens = 20480,
+        },
+      },
+      openai = {
+        endpoint = 'https://api.openai.com/v1',
+        model = 'gpt-4o',
+        extra_request_body = {
+          timeout = 30000,
+          temperature = 0.75,
+          max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+          --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+        },
+      },
+      ['openai-gpt-4o-mini'] = {
+        __inherited_from = 'openai',
+        model = 'gpt-4o-mini',
+      },
+      copilot = {
+        endpoint = 'https://api.githubcopilot.com',
+        model = 'gpt-4o-2024-11-20',
+        proxy = nil, -- [protocol://]host[:port] Use this proxy
+        allow_insecure = false, -- Allow insecure server connections
+        timeout = 30000,
+        extra_request_body = {
+          temperature = 0.75,
+          max_tokens = 20480,
+        },
+      },
+      gemini = {
+        endpoint = 'https://generativelanguage.googleapis.com/v1beta/models',
+        model = 'gemini-2.0-flash',
+        timeout = 30000,
+        use_ReAct_prompt = true,
+        extra_request_body = {
+          generationConfig = {
+            temperature = 0.75,
+          },
+        },
+      },
+      ollama = {
+        endpoint = 'http://127.0.0.1:11434',
+        timeout = 30000,
+        extra_request_body = {
+          options = {
+            temperature = 0.75,
+            num_ctx = 20480,
+            keep_alive = '5m',
+          },
+        },
+      },
+      -- DISABLED (workaround)
+      -- These would show up when I didn't want them to, so I set them to
+      -- inherit from a provider I'll never use, because fuck micro$oft
+      ['vertex'] = {
+        __inherited_from = 'cohere',
+      },
+      ['vertex_claude'] = {
+        __inherited_from = 'cohere',
+      },
+      ['bedrock'] = {
+        __inherited_from = 'cohere',
+      },
+      ['aihubmix'] = {
+        __inherited_from = 'cohere',
+      },
+      ['aihubmix-claude'] = {
+        __inherited_from = 'cohere',
       },
     },
     hints = {
@@ -126,6 +197,11 @@ return {
   'yetone/avante.nvim',
   event = 'VeryLazy',
   version = false, -- Never set this value to "*"! Never!
+  init = function()
+    vim.env.ANTHROPIC_API_KEY =
+      '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'
+    -- vim.env.OPENAI_API_KEY = ''
+  end,
   config = config,
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = 'make',
