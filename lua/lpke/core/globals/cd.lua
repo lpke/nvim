a95465927972100c310783d3f11ddcbd305edf0c
Luke <luke@lpdev.io>
2025-07-01 18:40:06 +1000

    progress on Lpke_cd function

--- lua/lpke/core/globals/cd.lua
+++ lua/lpke/core/globals/cd.lua
@@ -1,14 +1,29 @@
+-- checks if a path string is valid and exists in the filesystem
+-- path can be relative or absolute
+function Lpke_path_exists(path)
+  local stat = vim.uv.fs_stat(path)
+  return stat ~= nil
+end
+
 -- changes working directory to <target_path> for <target_scope ('global'|'tab'|'window')>
 -- returns final working directory as string
 function Lpke_cd(target_path, target_scope)
   -- TODO
   -- get a path and scope from the targets or fall back on defaults
-  -- use `find_upward_to_git_root_or_cwd` or `Lpke_find_git_root`
   -- use this to replace the `cd()` function in the oil config
 
-  local path = ''
-  local scope = ''
-  if path == nil or path == '' then
+  -- default path and scope if not provided or invalid
+  local path = Lpke_find_git_root() or vim.fn.getcwd()
+  local scope = 'global'
+  print(path)
+  print(scope)
+  if type(target_path) == 'string' and Lpke_path_exists(target_path) then
+    path = target_path
+  else
+    vim.notify(
+      'Lpke_cd: Invalid target_path provided, using current working directory.',
+      vim.log.levels.WARN
+    )
   end
 
   -- buffer type safety check
