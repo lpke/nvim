df9d9d4515ef0211c95365b7ffd0cff040b12724
Luke Perich <luke.p@montu.com.au>
2025-07-18 13:55:39 +1000

    back to mixed

--- lua/lpke/plugins/git/diffview.lua
+++ lua/lpke/plugins/git/diffview.lua
@@ -62,7 +62,7 @@ local function config()
       },
       merge_tool = {
         -- Config for conflicted files in diff views during a merge or rebase.
-        layout = 'diff1_plain',
+        layout = 'diff4_mixed',
         disable_diagnostics = false, -- Temporarily disable diagnostics for diff buffers while in the view.
         winbar_info = true, -- See |diffview-config-view.x.winbar_info|
       },
