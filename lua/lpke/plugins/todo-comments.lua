884cb0f87404bacac78009073cc9d94a6bc50284
Luke Perich <luke.p@montu.com.au>
2025-07-21 11:41:52 +1000

    Tweak todo highlights

--- lua/lpke/plugins/todo-comments.lua
+++ lua/lpke/plugins/todo-comments.lua
@@ -3,39 +3,38 @@ local function config()
   local helpers = require('lpke.core.helpers')
 
   local opts = {
-    signs = false, -- show icons in the signs column
+    signs = true, -- show icons in the signs column
     sign_priority = 8, -- sign priority
     -- keywords recognized as todo comments
     keywords = {
       FIX = {
-        -- icon = '\u{f024} ', -- icon used for the sign, and in search results
-        icon = '',
+        icon = '\u{f024}', -- icon used for the sign, and in search results
         color = 'error', -- can be a hex color, or a named color (see below)
         alt = { 'FIXME', 'BUG', 'FIXIT', 'ISSUE' }, -- a set of other keywords that all map to this FIX keywords
-        -- signs = false, -- configure signs for some keywords individually
+        signs = true, -- configure signs for some keywords individually
       },
-      TODO = { icon = '', color = 'info' },
-      TEMP = { icon = '', color = 'warning' },
-      HACK = { icon = '', color = 'warning' },
+      TODO = { icon = ' ', color = 'info' },
+      TEMP = { icon = '\u{f0ad}', color = 'warning', signs = true },
+      HACK = { icon = ' ', color = 'warning' },
       WARN = {
-        icon = '',
+        icon = ' ',
         color = 'warning',
         alt = { 'WARNING', 'XXX' },
       },
       PERF = {
-        icon = '',
+        icon = ' ',
         color = 'warning',
         alt = { 'OPTIM', 'PERFORMANCE', 'OPTIMIZE' },
       },
-      NOTE = { icon = '', color = 'hint', alt = { 'INFO' } },
+      NOTE = { icon = ' ', color = 'hint', alt = { 'INFO' } },
       TEST = {
-        icon = '',
+        icon = ' ',
         color = 'default',
         alt = { 'TESTING', 'PASSED', 'FAILED' },
       },
     },
     gui_style = {
-      fg = 'NONE', -- The gui style to use for the fg highlight group.
+      fg = 'BOLD', -- The gui style to use for the fg highlight group.
       bg = 'BOLD', -- The gui style to use for the bg highlight group.
     },
     merge_keywords = false, -- when true, custom keywords will be merged with the defaults
@@ -48,8 +47,8 @@ local function config()
       multiline_pattern = '^.', -- lua pattern to match the next multiline from the start of the matched keyword
       multiline_context = 10, -- extra lines that will be re-evaluated when changing a line
       before = '', -- "fg" or "bg" or empty
-      keyword = 'wide', -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
-      after = 'fg', -- "fg" or "bg" or empty
+      keyword = 'fg', -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
+      after = '', -- "fg" or "bg" or empty
       pattern = [[.*<(KEYWORDS)\s*:]], -- pattern or table of patterns, used for highlighting (vim regex)
       comments_only = true, -- uses treesitter to match keywords in comments only
       max_line_len = 400, -- ignore lines longer than this
