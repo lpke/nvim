c3a80ffdda75a9a8e69bf05b77172470c7320494
Luke Perich <luke.p@montu.com.au>
2025-07-31 17:22:33 +1000

    split `helpers/format.lua` into `format` and `info` files

--- lua/lpke/core/helpers/init.lua
+++ lua/lpke/core/helpers/init.lua
@@ -1,11 +1,12 @@
----@class lpke.core.helpers: lpke.core.helpers.misc, lpke.core.helpers.util, lpke.core.helpers.format, lpke.core.helpers.config, lpke.core.helpers.print
+---@class lpke.core.helpers: lpke.core.helpers.misc, lpke.core.helpers.util, lpke.core.helpers.config, lpke.core.helpers.print, lpke.core.helpers.info, lpke.core.helpers.format
 local M = {}
 
 local misc = require('lpke.core.helpers.misc')
 local util = require('lpke.core.helpers.util')
-local format = require('lpke.core.helpers.format')
 local config = require('lpke.core.helpers.config')
 local print = require('lpke.core.helpers.print')
+local info = require('lpke.core.helpers.info')
+local format = require('lpke.core.helpers.format')
 
 -- make all available via `lpke.core.helpers.<function>`
 for k, v in pairs(misc) do
@@ -14,13 +15,16 @@ end
 for k, v in pairs(util) do
   M[k] = v
 end
-for k, v in pairs(format) do
-  M[k] = v
-end
 for k, v in pairs(config) do
   M[k] = v
 end
 for k, v in pairs(print) do
+  M[k] = v
+end
+for k, v in pairs(info) do
+  M[k] = v
+end
+for k, v in pairs(format) do
   M[k] = v
 end
 
