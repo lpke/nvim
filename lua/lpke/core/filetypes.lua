3a8e51af8860e99c899f967fa399328c5062c1a0
Luke <luke@lpdev.io>
2026-04-26 16:08:36 +1000

    add automatic filetypes for chezmoi `.tmpl` files

--- lua/lpke/core/filetypes.lua
+++ lua/lpke/core/filetypes.lua
@@ -4,3 +4,18 @@ vim.filetype.add({
     mdx = 'mdx',
   },
 })
+
+-- handle chezmoi templates
+vim.filetype.add({
+  filename = {
+    ['dot_zshrc.tmpl'] = 'zsh',
+    ['dot_zshenv.tmpl'] = 'zsh',
+    ['dot_gitconfig.tmpl'] = 'gitconfig',
+  },
+  pattern = {
+    -- *.<filetype>.tmpl -> <filetype>
+    ['.*%.(%a+)%.tmpl'] = function(_, _, ext)
+      return ext
+    end,
+  },
+})
