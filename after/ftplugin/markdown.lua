69e25c0d4e493082a4a9c07bab746346b8ab79cd
Luke <luke@lpdev.io>
2026-06-28 20:52:47 +1000

    fix: make tabstop 2 spaces in markdown files to match everything else

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -2,3 +2,7 @@
 -- and comment-like lines during insert mode when `textwidth` is non-zero. Keep
 -- `textwidth` for manual formatting commands, but disable typing-time breaks.
 vim.opt_local.formatoptions:remove({ 't', 'c' })
+
+vim.opt_local.tabstop = 2
+vim.opt_local.softtabstop = 2
+vim.opt_local.shiftwidth = 2
