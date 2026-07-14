f3542ac00a104ba7ddfbdf3295980c97b9808aa4
Luke <luke@lpdev.io>
2026-07-14 15:19:10 +1000

    snippets: add utility `:<unicode-char>:` global snippets

--- lua/lpke/snippets/all.lua
+++ lua/lpke/snippets/all.lua
@@ -5,7 +5,41 @@ local ls, s, _s, sn, t, t_, i, f, d, rep, fmtc, fmta, fmt, sel, sel_q, sel_b, ex
   h.ls, h.s, h._s, h.sn, h.t, h.t_, h.i, h.f, h.d, h.rep, h.fmtc, h.fmta, h.fmt, h.sel, h.sel_q, h.sel_b, h.exp_conds
 -- stylua: ignore end
 
-return { -- all
+local symbol_snippets = {}
+local symbols = {
+  { ':close:', '🗙' },
+  { ':copyright:', '©' },
+  { ':check:', '✔' },
+  { ':cross:', '✖' },
+  { ':star:', '★' },
+  { ':arrow-up:', '↑' },
+  { ':arrow-down:', '↓' },
+  { ':arrow-left:', '←' },
+  { ':arrow-right:', '→' },
+  { ':sun:', '☀' },
+  { ':moon:', '☾' },
+  { ':ballot-checked:', '☑' },
+  { ':ballot-crossed:', '☒' },
+  { ':ballot-empty:', '☐' },
+  { ':warning:', '⚠' },
+  { ':bullet:', '•' },
+  { ':ellipsis:', '…' },
+  { ':degree:', '°' },
+  { ':ellipsis-vertical:', '⋮' },
+  { ':hamburger:', '☰' },
+  { ':refresh:', '↻' },
+  { ':settings:', '⚙' },
+  { ':triangle-right:', '▶' },
+  { ':triangle-down:', '▼' },
+  { ':chevron-down:', '⌄' },
+  { ':chevron-right:', '›' },
+}
+
+for _, symbol in ipairs(symbols) do
+  table.insert(symbol_snippets, s(symbol[1], t(symbol[2])))
+end
+
+return require('lpke.core.helpers').concat_arrs(symbol_snippets, { -- all
   _s(
     {
       trig = '```([%w%._+-]+)',
@@ -52,4 +86,4 @@ return { -- all
     trig = '[]',
     name = 'Inline array',
   }, fmt('[ <> ]', { i(1) })),
-}
+})
